e0a9f5a7c5de71ba393681da6bfd577f05a4c6bc
Christian Silvestri <christiansilvestri@live.it>
2022-09-23 16:11:34 +0200

    Add Passport with GitHub authentication strategy

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,14 @@ import "express-async-errors";
 import { validationErrorMiddleware } from "./lib/middleware/validation";
 import planetsRoutes from './routes/planets';
 import { initCorsMiddleware } from "./lib/middleware/cors";
+import { initSessionMiddleware } from "./lib/middleware/session";
+import { passport } from './lib/middleware/passport';
 
 const app = express();
 
+app.use(initSessionMiddleware());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.json());
 
